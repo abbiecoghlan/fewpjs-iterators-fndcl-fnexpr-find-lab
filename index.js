e5978f55d58b6c674f402d805192048c656d314c
“Abbie <abbie.coghlan@gmail.com>
2021-02-10 19:23:22 -0600

    done

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array){
+  let foundObj = array.find( element =>{  return element.result === "W" })
+  if (foundObj){ 
+    return foundObj.year
+  } else {
+    return undefined
+  }
+}
